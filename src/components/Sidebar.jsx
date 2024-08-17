8cf1ee43350e5dfc11aede636bc5bb83ae711686
mikepn02 <nzaberamikepeter@gmail.com>
2024-08-17 17:51:37 +0200

    profile

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,15 +3,18 @@ import Search from "../assets/search.svg";
 
 const Sidebar = ({ patients }) => {
   return (
-    <div className="bg-white rounded-lg w-[300px] p-3 t-[100px] fixed overflow-y-auto h-screen">
+    <div className="bg-white mt-6 rounded-lg w-[300px] p-4 fixed overflow-y-auto h-screen">
       <div className="flex mb-4 justify-between items-center">
-        <h2>Patients</h2>
+        <h2 className="text-xl font-semibold">Patients</h2>
         <img src={Search} alt="search" className="w-5 h-5" />
       </div>
       <div className="flex flex-col gap-4">
         {patients.map((patient, index) => (
-          <div key={index} className="flex items-center justify-between">
-            <div className="flex gap-2">
+          <div
+            key={index}
+            className="flex items-center justify-between bg-gray-100 p-2 rounded-lg"
+          >
+            <div className="flex gap-2 items-center">
               <div className="flex items-center justify-center rounded-full w-10 h-10">
                 <img
                   src={patient.profile_picture}
@@ -21,7 +24,9 @@ const Sidebar = ({ patients }) => {
               </div>
               <div>
                 <h4 className="text-sm font-medium">{patient.name}</h4>
-                <p className="text-xs text-gray-600">{patient.gender}, {patient.age}</p>
+                <p className="text-xs text-gray-600">
+                  {patient.gender}, {patient.age}
+                </p>
               </div>
             </div>
           </div>
